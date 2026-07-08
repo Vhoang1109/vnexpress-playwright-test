896313c535807c0e9bd7251d58f9244debb8de4e
Hoang <vhoang1109@gmail.com>
2026-07-08 09:47:43 +0700

    chore: update file test login

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -3,6 +3,8 @@ import { LoginPage } from "../../pages/login/login.page";
 
 test.describe("VNExpress Login", () => {
 
+    test.setTimeout(60000);
+    
   test("Login success", async ({ page }) => {
 
     const loginPage = new LoginPage(page);
@@ -38,4 +40,38 @@ test.describe("VNExpress Login", () => {
 
   });
 
+  test("Login fail", async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+
+    await test.step("Go to VNExpress", async () => {
+      await loginPage.gotoLoginPage();
+    });
+
+    await test.step("Open Login Popup", async () => {
+      await loginPage.openLoginPopup();
+    });
+
+    await test.step("Verify Email Textbox", async () => {
+      await loginPage.verifyEmailTextbox();
+    });
+
+    await test.step("Fill Email", async () => {
+      await loginPage.fillEmail("vhoang1928@gmail.com");
+    });
+
+
+    await test.step("Verify Password Textbox", async () => {
+      await loginPage.verifyPasswordTextbox();
+    });
+
+    await test.step("Fill Password", async () => {
+      await loginPage.fillPassword('');
+    });
+
+    await test.step("Click Login", async () => {
+      await loginPage.clickLogin();
+    });
+
+  });
 });
