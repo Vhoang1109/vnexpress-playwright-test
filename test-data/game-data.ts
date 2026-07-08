e760362058983be8c9f9a753cc93592e3c108fb8
Hoang <vhoang1109@gmail.com>
2026-07-09 06:52:21 +0700

    feat: update file game data

---
+++ test-data/game-data.ts
@@ -0,0 +1,19 @@
+export type GameData = {
+    gameName: string;
+    gameSlug: string;
+};
+
+export const gameData01: GameData = {
+    gameName: "Xếp chữ",
+    gameSlug: "xep-chu",
+};
+
+export const gameData02: GameData = {
+    gameName: "Chính tả",
+    gameSlug: "chinh-ta",
+};
+
+export const gameData03: GameData = {
+    gameName: "Sudoku",
+    gameSlug: "sudoku",
+};
