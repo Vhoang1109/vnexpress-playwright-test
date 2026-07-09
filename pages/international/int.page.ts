08468d3926c29a7e194df768aa58eb40fe36295e
Hoang <vhoang1109@gmail.com>
2026-07-10 02:54:24 +0700

    feat: update file international page

--- pages/international/int.page.ts
+++ pages/international/int.page.ts
@@ -8,10 +8,11 @@ export class International {
     }
 
     async gotoHomePage() {
-   await this.page.goto("https://vnexpress.net/", {
-        waitUntil: "domcontentloaded",
-        timeout: 60000,
-    });    }
+        await this.page.goto("https://vnexpress.net/", {
+            waitUntil: "domcontentloaded",
+            timeout: 60000,
+        });
+    }
     async clickInternational() {
         await this.page.getByTitle("VnExpress International").click();
     }
