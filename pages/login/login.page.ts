de13440a6d36760bb5229e557997e2bd77094e84
Hoang <vhoang1109@gmail.com>
2026-07-10 07:18:52 +0700

    chore: update file login page

--- pages/login/login.page.ts
+++ pages/login/login.page.ts
@@ -1,29 +1,23 @@
 import { expect, FrameLocator, Page } from "@playwright/test";
+import { BasePage } from "../base-page/base.page";
 
-export class LoginPage {
-    readonly page: Page;
-    readonly loginFrame: FrameLocator;
+export class LoginPage extends BasePage {
+    loginFrame: FrameLocator;
 
     constructor(page: Page) {
-        this.page = page;
+        super(page);
+
         this.loginFrame = page.locator("iframe").nth(2).contentFrame();
     }
 
+    // Navigation
     async gotoLoginPage() {
-        await this.page.goto("https://vnexpress.net/", {
-            timeout: 60000,
-            waitUntil: "domcontentloaded",
-        });
+        await this.gotoHomePage();
     }
 
+    // Actions
     async openLoginPopup() {
         await this.page.getByRole("link", { name: "Đăng nhập" }).click();
-    }
-
-    async verifyEmailTextbox() {
-        await expect(
-            this.loginFrame.getByRole("textbox", { name: "Nhập Email của bạn" })
-        ).toBeVisible();
     }
 
     async fillEmail(email: string) {
@@ -32,11 +26,9 @@ export class LoginPage {
         });
 
         await expect(emailTextbox).toBeVisible();
-
         await emailTextbox.click();
         await emailTextbox.clear();
         await emailTextbox.pressSequentially(email, { delay: 80 });
-
         await expect(emailTextbox).toHaveValue(email);
     }
 
@@ -44,14 +36,9 @@ export class LoginPage {
         const continueBtn = this.loginFrame.getByRole("button", {
             name: "Tiếp tục",
         });
+
         await expect(continueBtn).toBeEnabled();
         await continueBtn.click();
-    }
-
-    async verifyPasswordTextbox() {
-        await expect(
-            this.loginFrame.getByRole("textbox", { name: "Mật khẩu" })
-        ).toBeVisible();
     }
 
     async fillPassword(password: string) {
@@ -70,5 +57,14 @@ export class LoginPage {
 
         await expect(loginBtn).toBeEnabled();
         await loginBtn.click();
+    }
+
+    // Verification
+    async verifyEmailTextbox() {
+        await expect(this.loginFrame.getByRole("textbox", { name: "Nhập Email của bạn", })).toBeVisible();
+    }
+
+    async verifyPasswordTextbox() {
+        await expect(this.loginFrame.getByRole("textbox", { name: "Mật khẩu", })).toBeVisible();
     }
 }
