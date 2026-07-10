7b833ad5b8862e933ae1d21cce5063e0762a14ee
Hoang <vhoang1109@gmail.com>
2026-07-10 09:55:25 +0700

    chore: update file educate page

--- pages/education/edu.page.ts
+++ pages/education/edu.page.ts
@@ -3,31 +3,37 @@ import { BasePage } from "../base-page/base.page";
 
 export class EduPage extends BasePage {
 
+    eduLocatorXpath: string = "(//a[@title='Giáo dục'])[1]";
+    eduAdmissionsXpath: string = "(//a[@title='Tuyển sinh'])[1]";
+    eduExamScoreXpath: string = "//a[@title='Điểm thi']";
+    eduSbdXpath: string = "//input[@id='tc-search-input']";
+    eduBtnXpath: string = "//button[@type='button']";
+
     constructor(page: Page) {
         super(page);
     }
 
     // Actions
     async clickEducate() {
-        await this.page.locator("(//a[@title='Giáo dục'])[1]").click();
+        await this.page.locator(this.eduLocatorXpath).click();
     }
 
     async clickAdmissions() {
-        await this.page.locator("(//a[@title='Tuyển sinh'])[1]").click();
+        await this.page.locator(this.eduAdmissionsXpath).click();
     }
 
     async clickExamScore() {
-        await this.page.locator("//a[@title='Điểm thi']").click();
+        await this.page.locator(this.eduExamScoreXpath).click();
     }
 
     async searchExamNumber(sbd: string) {
-        const textbox = this.page.locator("//input[@id='tc-search-input']");
+        const textbox = this.page.locator(this.eduSbdXpath);
 
         await textbox.fill(sbd);
     }
 
     async clickViewResult() {
-        await this.page.locator("//button[@type='button']").click();
+        await this.page.locator(this.eduBtnXpath).click();
     }
 
     // Verification
