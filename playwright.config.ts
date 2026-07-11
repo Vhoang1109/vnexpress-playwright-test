e9f88633b2b385e0bb752040b514843588acb0f9
Hoang <vhoang1109@gmail.com>
2026-07-11 15:07:59 +0700

    chore: update file  config

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['iPhone 15 Pro Max'] },
     },
 
     // {
