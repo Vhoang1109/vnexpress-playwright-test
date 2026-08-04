85133a8546ec1d5aca150e921f042d19d4c24a2f
Hoang <vhoang1109@gmail.com>
2026-08-04 09:36:30 +0700

    delete file code gen

--- tests/non-with-pom/demo.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://diemthi.vnexpress.net/?_gl=1*vrdatv*_gcl_au*MzU2ODQ4MzcwLjE3ODM1NzQ3Mzg.');
-  await page.getByRole('button', { name: 'Xem kết quả' }).click();
-  await page.getByRole('textbox', { name: 'Nhập số báo danh' }).click();
-  await page.getByRole('textbox', { name: 'Nhập số báo danh' }).fill('');
-  await page.getByRole('button', { name: 'Xem kết quả' }).click();
-});
